a2fefaecfc34884893eac7aa09430823e487657d
Tomasz Ducin <tomasz@ducin.it>
2019-08-20 17:46:36 +0200

    improving memoize

--- test/memoize.spec.js
+++ test/memoize.spec.js
@@ -6,6 +6,8 @@ const assert = chai.assert
 
 const sinon = require('sinon')
 
+const { Calculator } = require('../src/calc')
+
 describe('Memoize decorator', () => {
   it('should return the same result as the wrapped function', () => {
     const square = (n) => n**2
@@ -54,5 +56,38 @@ describe('Memoize decorator', () => {
     result3.should.equal(25)
   })
 
-  // spying method object
+  it('should memoize functions which operate on various number of parameters', () => {
+    // spying method object
+    const spy = sinon.spy(Calculator, 'add')
+    const memoizedAdd = memoize(Calculator.add)
+
+    sinon.assert.notCalled(spy)
+
+    const result1 = memoizedAdd(1, 2)
+    const result2 = memoizedAdd(4, 5)
+
+    sinon.assert.calledTwice(spy)
+
+    const result3 = memoizedAdd(4, 5)
+    const result4 = memoizedAdd(4, 5)
+    const result5 = memoizedAdd(1, 2)
+
+    sinon.assert.callCount(spy, 2)
+    result1.should.equal(3)
+    result2.should.equal(9)
+    result3.should.equal(9)
+    result4.should.equal(9)
+    result5.should.equal(3)
+    sinon.assert.calledWith(spy, 1, 2)
+    sinon.assert.calledWith(spy, 4, 5)
+
+    // const concat = (arr1, arr2) => arr1.concat(arr2)
+    // const memoizedConcat = memoize(concat)
+    // const result6 = memoizedConcat([], [1, 2]) // -> [1,2]
+    // const result7 = memoizedConcat([], [1, 2])
+
+    // sinon.assert.threw(spy)
+    // sinon.assert.threw(spy())
+
+  })
 })
