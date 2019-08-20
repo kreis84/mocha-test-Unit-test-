71f9421de7c59dc79f8bb9769fb54f801ac0d296
Tomasz Ducin <tomasz@ducin.it>
2019-08-20 13:35:12 +0200

    initial commit

---
+++ test/calc.spec.js
@@ -0,0 +1,19 @@
+const assert = require('assert')
+
+const { Calculator } = require('../src/calc')
+
+describe('Calculator', () => {
+
+  it('should add numbers', () => {
+    // assert.expectation(3)
+
+    assert.equal(Calculator.add(2,2), 4)
+    // assert.equal(Calculator.add(2,2), 5, "błędny wynik dodawania")
+    // assert.equal(Calculator.add(2,2), 6, "błędny wynik dodawania")
+
+    // if (2 !== 3) {
+    //   throw new Error('2 not equal to 3')
+    // }
+  })
+
+})
